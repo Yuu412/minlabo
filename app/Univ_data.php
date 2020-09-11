c8141888450f1df27374bfedbe72fdead6751996
TOMOFUMI-KONDO <ugax2kontomo0314@gmai.com>
2020-09-11 22:24:29 +0900

    ide-helperの作成

--- app/Univ_data.php
+++ app/Univ_data.php
@@ -4,6 +4,26 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Univ_data
+ *
+ * @property int $id
+ * @property string $pre_name
+ * @property string $univ_name
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Laboratory[] $Laboratory
+ * @property-read int|null $laboratory_count
+ * @method static \Illuminate\Database\Eloquent\Builder|Univ_data newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Univ_data newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Univ_data query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Univ_data whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Univ_data whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Univ_data wherePreName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Univ_data whereUnivName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Univ_data whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Univ_data extends Model
 {
   protected $table = 'univ_data';
--- app/User.php
+++ app/User.php
@@ -6,6 +6,48 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * App\User
+ *
+ * @property int $id
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string $password
+ * @property int $role
+ * @property string $univ_name
+ * @property string $faculty_name
+ * @property string $department_name
+ * @property string $lab_name
+ * @property string $token
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property int $email_verified
+ * @property string|null $email_verify_token
+ * @property int $status
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereDepartmentName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerified($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifyToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereFacultyName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereLabName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRole($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUnivName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class User extends Authenticatable 
 {
     use Notifiable;
